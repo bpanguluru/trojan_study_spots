d07caa26f24848150b5dff1841a67ee9561e6307
Allena Villanueva <allenalindsay@gmail.com>
2024-04-16 20:31:18 -0700

    4/16 AV 8:31

--- studyspots/src/main/webapp/script.js
+++ studyspots/src/main/webapp/script.js
@@ -2,18 +2,8 @@
  * 
  */
 
-// Function to change image
-function changeImage(event) {
-    // Get the value of the selected radio button
-    const selectedValue = event.target.value;
-
-    // Update image source based on selected value
-    if (selectedValue === 'blank-tommy.svg') {
-        blank-tommy.src = 'filled-tommy.svg'; // Change 'image1.jpg' to the path of your first image
-    } 
+function changeImageClass(fromClass, toClass) {
+    var image = document.getElementById('image');
+    image.classList.remove(fromClass);
+    image.classList.add(toClass);
 }
-
-// Attach change event listener to each radio button
-radioButtons.forEach(radioButton => {
-    radioButton.addEventListener('change', changeImage);
-});

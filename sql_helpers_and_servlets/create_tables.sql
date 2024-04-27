68ac38670bc83190f8b99792e7ea9a465714ea82
Bhargav Panguluru <pangulur@usc.edu>
2024-04-26 21:34:51 -0700

    Update create_tables.sql

--- sql_helpers_and_servlets/create_tables.sql
+++ sql_helpers_and_servlets/create_tables.sql
@@ -4,6 +4,7 @@ USE TrojanStudy;
 
 CREATE TABLE Users (
     userID INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );

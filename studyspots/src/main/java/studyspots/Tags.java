865c93c97f871cbb650bcf0bdecc68c4bb44a29c
Ellen Landrum <elandrum@usc.edu>
2024-04-29 12:15:30 -0700

    added Tags class

---
+++ studyspots/src/main/java/studyspots/Tags.java
@@ -0,0 +1,10 @@
+package studyspots;
+
+public class Tags {
+	public final String[] tags = {"Loud", "Quiet", "Silent", "Outside", "Natural Light", 
+		 "No Windows", "Much Seating", "Limited Seating", "Comfortable",
+		 "Alternative Seating", "Clean", "Outlets", "No Outlets", "White boards",
+		 "Chalk boards", "Book stacks", "Good for groups", "No food Allowed", "Food Nearby",
+		 "Good For Mornings", "Good For Afternoon", "Good For Nights"};
+	public final int numTags = 22;
+}
